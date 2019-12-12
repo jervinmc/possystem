eed7b05cd88c16191f42b4cfced716334b177830
Hey Hey <jervinmac@yahoo.com>
2019-12-12 17:00:23 +0800

    kjh21hjb jkae

--- lib/Signin.dart
+++ lib/Signin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:possystem/homepage.dart';
+import 'package:possystem/utils.dart';
 import 'fadeAnimation.dart';
 import 'transition.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,9 +14,60 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-
+//variable
   TextEditingController username = new TextEditingController();
   TextEditingController password = new TextEditingController();
+  //function
+  Future<void> signinFunction(BuildContext context,int x) {
+        
+  return showDialog<void>(   
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius:BorderRadius.circular(15)
+        ),
+        child: AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      backgroundColor: Colors.white,
+        title:Center( 
+          child: textCustom("Password/Username is not recognized.", 25, Colors.red, "style",),),
+        content:Text(""),
+        actions: <Widget>[
+           Center(
+             child:Container(
+               width: 300,
+               child: Center(
+                 child:  Row(
+                   mainAxisAlignment: MainAxisAlignment.end,
+                 children: <Widget>[
+                   new OutlineButton(
+      borderSide: BorderSide(
+            color: Colors.red, //Color of the border
+            style: BorderStyle.solid, //Style of the border
+            width: 2, //width of the border
+          ),
+    color:Colors.red,
+  child: new textCustom("OK",25,Colors.red,""),
+  onPressed: (){
+    
+  Navigator.of(context).pop();
+  },
+  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+),
+
+
+                 ],
+               ),
+               )
+             )
+           )
+        ],
+      ),
+      );
+    },
+  );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -102,17 +154,23 @@ class _SignInState extends State<SignIn> {
                     minWidth: 250.0,
                     height: 35.0,
                     onPressed: ()async{
-                      http.Response response=await http.get(Uri.encodeFull("http://192.168.1.115:424/api/User/GetAll"),headers: {
+                    /*  http.Response response=await http.get(Uri.encodeFull("http://192.168.1.115:424/api/User/GetAll"),headers: {
                      "Accept":"application/json"
 
      });
+     int a=0;
      var reviewdata = json.decode(response.body);
-     for (int x = 0; x < reviewdata.length; x++) {
+    for (int x = 0; x < reviewdata.length; x++) {
        if (username.text == reviewdata[x]['firstname'] ){
-         
+              a=1;
                      Navigator.push(context, SlideRightRoute(widget: Homepage()));
+
        }
-     }
+     }*/
+    //if(a==0){
+    //    signinFunction(context, 1);
+   // }
+   Navigator.push(context, SlideRightRoute(widget: Homepage()));
 
                     },
                     child: Text("SIGN IN",
